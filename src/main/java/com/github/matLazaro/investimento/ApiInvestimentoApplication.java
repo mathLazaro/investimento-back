7e2d8d91d5054b9928e2358039cfcffe5c90057c
Matheus Lázaro de Lima <matheus.lazaro2002@gmail.com>
2025-02-17 14:54:55 -0300

    init

---
+++ src/main/java/com/github/matLazaro/investimento/ApiInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.github.matLazaro.investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matLazaro/investimento/ApiInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matLazaro.investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
