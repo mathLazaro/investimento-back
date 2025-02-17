ad993f65fb6d31ecee134ac7f2c8913af870a4b9
Matheus Lázaro de Lima <matheus.lazaro2002@gmail.com>
2025-02-17 19:12:31 -0300

    project config

--- src/main/java/com/github/matLazaro/investimento/ApiInvestimentoApplication.java
+++ src/main/java/com/github/matLazaro/investimento/ApiInvestimentoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiInvestimentoApplication {
 
+	//TODO - corrigir swagger
 	public static void main(String[] args) {
 		SpringApplication.run(ApiInvestimentoApplication.class, args);
 	}
